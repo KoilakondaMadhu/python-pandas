34b9eb3ee0cd928224401e4675d3bb69a76c7e5b
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-11 19:54:19 +0530

    Update 2877. Create a DataFrame from List.py

--- 2877. Create a DataFrame from List.py
+++ 2877. Create a DataFrame from List.py
@@ -15,3 +15,23 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     
     # Returning the resulting DataFrame
     return result
+
+# Input:
+# student_data:
+# [
+#   [1, 15],
+#   [2, 11],
+#   [3, 11],
+#   [4, 20]
+# ]
+# Output:
+# +------------+-----+
+# | student_id | age |
+# +------------+-----+
+# | 1          | 15  |
+# | 2          | 11  |
+# | 3          | 11  |
+# | 4          | 20  |
+# +------------+-----+
+# Explanation:
+# A DataFrame was created on top of student_data, with two columns named student_id and age.
