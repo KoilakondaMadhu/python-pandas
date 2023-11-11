6a17cf9b3f78aa722c927a0404d21cd772a70b4c
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-11 19:55:37 +0530

    Create 2879. Display the First Three Rows.py

---
+++ 2879. Display the First Three Rows.py
@@ -0,0 +1,49 @@
+# Importing the pandas library and renaming it as pd for convenience
+import pandas as pd
+
+# Defining a function named selectFirstRows that takes a pandas DataFrame as input
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    # Using the head method to select the first 3 rows of the DataFrame
+    selected_rows = employees.head(3)
+    
+    # Returning the resulting DataFrame
+    return selected_rows
+
+
+# DataFrame: employees
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | employee_id | int    |
+# | name        | object |
+# | department  | object |
+# | salary      | int    |
+# +-------------+--------+
+# Write a solution to display the first 3 rows of this DataFrame.
+
+ 
+
+# Example 1:
+
+# Input:
+# DataFrame employees
+# +-------------+-----------+-----------------------+--------+
+# | employee_id | name      | department            | salary |
+# +-------------+-----------+-----------------------+--------+
+# | 3           | Bob       | Operations            | 48675  |
+# | 90          | Alice     | Sales                 | 11096  |
+# | 9           | Tatiana   | Engineering           | 33805  |
+# | 60          | Annabelle | InformationTechnology | 37678  |
+# | 49          | Jonathan  | HumanResources        | 23793  |
+# | 43          | Khaled    | Administration        | 40454  |
+# +-------------+-----------+-----------------------+--------+
+# Output:
+# +-------------+---------+-------------+--------+
+# | employee_id | name    | department  | salary |
+# +-------------+---------+-------------+--------+
+# | 3           | Bob     | Operations  | 48675  |
+# | 90          | Alice   | Sales       | 11096  |
+# | 9           | Tatiana | Engineering | 33805  |
+# +-------------+---------+-------------+--------+
+# Explanation: 
+# Only the first 3 rows are displayed.
