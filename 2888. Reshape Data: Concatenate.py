6375b9aa1eabc579b91509d4c94d8b610e775344
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-12 20:34:57 +0530

    Create 2888. Reshape Data: Concatenate.py

---
+++ 2888. Reshape Data: Concatenate.py
@@ -0,0 +1,107 @@
+
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    # Concatenate df1 and df2 along the rows (axis=0)
+    result = pd.concat([df1, df2], axis=0)
+    
+    # Return the concatenated DataFrame
+    return result
+
+
+
+# Easy
+# Companies
+# Hint
+# DataFrame df1
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# +-------------+--------+
+
+# DataFrame df2
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# +-------------+--------+
+
+# Write a solution to concatenate these two DataFrames vertically into one DataFrame.
+
+# The result format is in the following example.
+
+ 
+
+# Example 1:
+
+# Input:
+# df1
+# +------------+---------+-----+
+# | student_id | name    | age |
+# +------------+---------+-----+
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# +------------+---------+-----+
+# df2
+# +------------+------+-----+
+# | student_id | name | age |
+# +------------+------+-----+
+# | 5          | Leo  | 7   |
+# | 6          | Alex | 7   |
+# +------------+------+-----+
+# Output:
+# +------------+---------+-----+
+# | student_id | name    | age |
+# +------------+---------+-----+
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# | 5          | Leo     | 7   |
+# | 6          | Alex    | 7   |
+# +------------+---------+-----+
+# Explanation:
+# The two DataFramess are stacked vertically, and their rows are combined.
+
+
+
+  
+# Case 1
+# Input
+# df1 =
+# | student_id | name    | age |
+# | ---------- | ------- | --- |
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# df2 =
+# | student_id | name | age |
+# | ---------- | ---- | --- |
+# | 5          | Leo  | 7   |
+# | 6          | Alex | 7   |
+# Output
+# | student_id | name    | age |
+# | ---------- | ------- | --- |
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# | 5          | Leo     | 7   |
+# | 6          | Alex    | 7   |
+# Expected
+# | student_id | name    | age |
+# | ---------- | ------- | --- |
+# | 1          | Mason   | 8   |
+# | 2          | Ava     | 6   |
+# | 3          | Taylor  | 15  |
+# | 4          | Georgia | 17  |
+# | 5          | Leo     | 7   |
+# | 6          | Alex    | 7   |
