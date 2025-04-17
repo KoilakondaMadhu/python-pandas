91915a7c9c15a490431c5ee42bc3215f9c681aca
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2025-04-17 10:37:26 +0530

    Create 570. Managers with at Least 5 Direct Reports.py

---
+++ 570. Managers with at Least 5 Direct Reports.py
@@ -0,0 +1,12 @@
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    managers = employee.groupby(
+        'managerId', as_index=False
+    ).agg(
+        reporting=('id', 'count'),
+    ).query(
+        '5 <= reporting'
+    )['managerId']
+
+    return employee[
+        employee['id'].isin(managers)
+    ][['name']]
